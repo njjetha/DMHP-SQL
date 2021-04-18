b817bd571e8eeb7b56a4f36b43d87c98e3a129f4
njjetha <neeraj.jetha@iiitb.org>
2021-04-19 00:02:00 +0530

    Droppped FacilityType column no requirement and changed the Mnc FacilityTypeId

--- orderingReportData.sql
+++ orderingReportData.sql
@@ -19,7 +19,7 @@ Alter Table DMHP.ReportData DROP column MncHospitalId;
 SET SQL_SAFE_UPDATES=0;
 update DMHP.ReportData SET TalukaId=274 where ReportId=80682 and TalukaId=0;
 update DMHP.ReportData SET TalukaId=180 where ReportId=80882 and TalukaId=0;
-update DMHP.ReportData SET FacilityType=1 where TalukaId=274 or TalukaId=180;
+--update DMHP.ReportData SET FacilityType=1 where TalukaId=274 or TalukaId=180;
 SET SQL_SAFE_UPDATES=1;
 
 DROP TABLE DMHP.Reportdata_restructured;
@@ -27,8 +27,8 @@ DROP TABLE DMHP.Reportdata_restructured;
 
 
 SET SQL_SAFE_UPDATES=0;
-update DMHP.ReportData SET FacilityTypeId=1,FacilityType='PHC' where FacilityId in (select distinct FacilityId from tbl_facility where FacilityTypeId=1);
-update DMHP.ReportData SET FacilityTypeId=2,FacilityType='CHC' where FacilityId in (select distinct FacilityId from tbl_facility where FacilityTypeId=2);
-update DMHP.ReportData SET FacilityTypeId=3,FacilityType='Taluka Hospital' where FacilityId in (select distinct FacilityId from tbl_facility where FacilityTypeId=3);
-update DMHP.ReportData SET FacilityTypeId=4,FacilityType='MnC' where FacilityId is NULL;
+update DMHP.ReportData SET FacilityTypeId=1 where FacilityId in (select distinct FacilityId from tbl_facility where FacilityTypeId=1);
+update DMHP.ReportData SET FacilityTypeId=2 where FacilityId in (select distinct FacilityId from tbl_facility where FacilityTypeId=2);
+update DMHP.ReportData SET FacilityTypeId=3 where FacilityId in (select distinct FacilityId from tbl_facility where FacilityTypeId=3);
+update DMHP.ReportData SET FacilityTypeId=8 where FacilityId is NULL;
 SET SQL_SAFE_UPDATES=1;
