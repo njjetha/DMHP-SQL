100855576087381554f8227fb4090612696c3ef6
njjetha <neeraj.jetha@iiitb.org>
2021-04-18 23:12:17 +0530

    ReportData table FacilityType and FacilityTypeID added

--- ReportDataRestructured.sql
+++ ReportDataRestructured.sql
@@ -1,5 +1,5 @@
 CREATE TABLE DMHP.Reportdata_restructured as
-	SELECT ReportId,StateId,DistrictId,TalukaId,MncHospitalId,PhcId,ChcId,1 as FacilityType,FacilityId,ReportingMonthyear,'old' as VisitType,'M' as Gender,'IP' as ConsultType,
+	SELECT ReportId,StateId,DistrictId,TalukaId,MncHospitalId,1 as FacilityTypeId,'Primary Health Center' 				FacilityType,FacilityId,ReportingMonthyear,'old' as VisitType,'M' as Gender,'OP' as ConsultType,
 		old_smd_male as SMD,old_cmd_male as CMD,old_male_suicidecases as SuicideAttempts,old_o1_male as Epilepsy,
         old_alcohal_male as AlcoholSubstanceAbuse,old_o5_male as Dementia,old_o2_male as DevelopmentalDisorders,
         old_o3_male as BehaviouralDisorders,old_o4_male as EmotionalDisorders,old_psychiatricdisorders_male as PsychiatricDisorders,
@@ -9,7 +9,7 @@ CREATE TABLE DMHP.Reportdata_restructured as
         
 												UNION ALL
                                                 
-	SELECT ReportId,StateId,DistrictId,TalukaId,MncHospitalId,PhcId,ChcId,1 as FacilityType,FacilityId,ReportingMonthyear,'old' as VisitType,'F' as Gender,'IP' as ConsultType,
+	SELECT ReportId,StateId,DistrictId,TalukaId,MncHospitalId,1 as FacilityTypeId,'Primary Health Center' 				FacilityType,FacilityId,ReportingMonthyear,'old' as VisitType,'F' as Gender,'OP' as ConsultType,
 		old_smd_female as SMD,old_cmd_female as CMD ,old_female_suicidecases as SuicideAttempts,old_o1_female as Epilepsy,
         old_alcohal_female as AlcoholSubstanceAbuse,old_o5_female as Dementia,old_o2_female as DevelopmentalDisorders,
         old_o3_female as BehaviouralDisorders,old_o4_female as EmotionalDisorders,old_psychiatricdisorders_female as PsychiatricDisorders, 
@@ -19,7 +19,7 @@ CREATE TABLE DMHP.Reportdata_restructured as
         
 												UNION ALL
         
-	SELECT ReportId,StateId,DistrictId,TalukaId,MncHospitalId,PhcId,ChcId,1 as FacilityType,FacilityId,ReportingMonthyear,'new' as VisitType,'M' as Gender,'IP' as ConsultType,
+	SELECT ReportId,StateId,DistrictId,TalukaId,MncHospitalId,1 as FacilityTypeId,'Primary Health Center' 				FacilityType,FacilityId,ReportingMonthyear,'new' as VisitType,'M' as Gender,'OP' as ConsultType,
 		new_smd_male as SMD,new_cmd_male as CMD,new_male_suicidecases as SuicideAttempts,new_o1_male as Epilepsy,
         new_alcohal_male as AlcoholSubstanceAbuse,new_o5_male as Dementia,new_o2_male as DevelopmentalDisorders, 
         new_o3_male as BehaviouralDisorders,new_o4_male as EmotionalDisorders,new_psychiatricdisorders_male as PsychiatricDisorders, 
@@ -29,7 +29,7 @@ CREATE TABLE DMHP.Reportdata_restructured as
         
 												UNION ALL
                         
-	SELECT ReportId,StateId,DistrictId,TalukaId,MncHospitalId,PhcId,ChcId,1 as FacilityType,FacilityId,ReportingMonthyear,'new' as VisitType,'F' as Gender,'IP' as ConsultType,
+	SELECT ReportId,StateId,DistrictId,TalukaId,MncHospitalId,1 as FacilityTypeId,'Primary Health Center' 			         FacilityType,FacilityId,ReportingMonthyear,'new' as VisitType,'F' as Gender,'OP' as ConsultType,
 		new_smd_female as SMD,new_cmd_female as CMD,new_female_suicidecases as SuicideAttempts,new_o1_female as Epilepsy,
         new_alcohal_female as AlcoholSubstanceAbuse,new_o5_female as Dementia,new_o2_female as DevelopmentalDisorders, 
         new_o3_female as BehaviouralDisorders,new_o4_female as EmotionalDisorders,new_psychiatricdisorders_female as PsychiatricDisorders,
@@ -39,7 +39,7 @@ CREATE TABLE DMHP.Reportdata_restructured as
 						
 												UNION ALL
                         
-	SELECT ReportId,StateId,DistrictId,TalukaId,MncHospitalId,PhcId,ChcId,1 as FacilityType,FacilityId,ReportingMonthyear,'N' as VisitType,'N' as Gender,'OP' as ConsultType,
+	SELECT ReportId,StateId,DistrictId,TalukaId,MncHospitalId,1 as FacilityTypeId,'Primary Health Center'                 	   		FacilityType,FacilityId,ReportingMonthyear,'N' as VisitType,'N' as Gender,'IP' as ConsultType, 
 		InPatient_1 as SMD,InPatient_2 as CMD,InPatient_6 as SuicideAttempts,InPatient_7 as Epilepsy,
         InPatient_3 as AlcoholSubstanceAbuse, InPatient_11 as Dementia,InPatient_8 as DevelopmentalDisorders, 
         InPatient_9 as BehaviouralDisorders,InPatient_10 as EmotionalDisorders,InPatient_5 as PsychiatricDisorders,
--- orderingReportData.sql
+++ orderingReportData.sql
@@ -5,15 +5,15 @@ CREATE table ReportData as
 
 
 SET SQL_SAFE_UPDATES=0;
-update DMHP.ReportData SET FacilityType=2 where MncHospitalId is not NULL;
+-- update DMHP.ReportData SET FacilityType=2 where MncHospitalId is not NULL;
 update DMHP.ReportData SET TalukaId=MncHospitalId where TalukaId=0;
-update DMHP.ReportData SET FacilityType=3 where PhcId !=0;
-update DMHP.ReportData SET FacilityType=4 where ChcId !=0;
+-- update DMHP.ReportData SET FacilityType=3 where PhcId !=0;
+-- update DMHP.ReportData SET FacilityType=4 where ChcId !=0;
 SET SQL_SAFE_UPDATES=1;
 
 Alter Table DMHP.ReportData DROP column MncHospitalId;
-Alter Table DMHP.ReportData DROP column PhcId;
-Alter Table DMHP.ReportData DROP column ChcId;
+-- Alter Table DMHP.ReportData DROP column PhcId;
+-- Alter Table DMHP.ReportData DROP column ChcId;
 
 
 SET SQL_SAFE_UPDATES=0;
@@ -25,7 +25,10 @@ SET SQL_SAFE_UPDATES=1;
 DROP TABLE DMHP.Reportdata_restructured;
 
 
+
 SET SQL_SAFE_UPDATES=0;
-update DMHP.ReportData SET ConsultType='OP' where ConsultType='IP';
-update DMHP.ReportData SET ConsultType='IP' where Gender='N' and VisitType='N';
+update DMHP.ReportData SET FacilityTypeId=1,FacilityType='PHC' where FacilityId in (select distinct FacilityId from tbl_facility where FacilityTypeId=1);
+update DMHP.ReportData SET FacilityTypeId=2,FacilityType='CHC' where FacilityId in (select distinct FacilityId from tbl_facility where FacilityTypeId=2);
+update DMHP.ReportData SET FacilityTypeId=3,FacilityType='Taluka Hospital' where FacilityId in (select distinct FacilityId from tbl_facility where FacilityTypeId=3);
+update DMHP.ReportData SET FacilityTypeId=4,FacilityType='MnC' where FacilityId is NULL;
 SET SQL_SAFE_UPDATES=1;
